b24aaa514dec1f0e4b12144f4d4ab0a30dd1e6cf
Raushan kumar <86125144+raushan9jnv@users.noreply.github.com>
2024-03-23 10:04:44 +0530

    blog app working

--- app.py
+++ app.py
@@ -1,4 +1,40 @@
 import streamlit as st
+import google.generativeai as genai
+from apikey import google_gemini_api_key
+
+# Set up the model
+genai.configure(api_key= google_gemini_api_key)
+
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+#setting up our model
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
 
 #set app to wide mode
 st.set_page_config(layout='wide')
@@ -8,25 +44,8 @@ st.title('BlogCraft: Your AI Writing Companion')
 
 #create a subheader
 st.subheader("Now you can craft perfect blogs with the help of AI- Blogcraft is your new AI blog companion")
-blog = '''Artificial intelligence (AI) is changing the way we live and work, and chatbots are playing a significant role in that change. Artificial intelligence has come a long way in recent years, and it’s no surprise that AI chatbots are becoming more and more prevalent in our daily lives. With AI chatbots, we can quickly and easily find answers to our questions, get help with tasks, and even just have a conversation. Two of the most popular AI chatbots out there today are BARD and ChatGPT. Both of these chatbots are designed to make our lives easier, but what are the key differences between them and which one is the best?
-        In this blog, we’ll explore the world of AI chatbots and find out what sets BARD and ChatGPT apart from each other.
-
-Outline:
-Overview of ChatGPT and BARD
-Key differences between ChatGPT and BARD
-Concerns with both AI chatbots
-Use cases for ChatGPT and BARD
-Which one is the best?
-Final thoughts
-#1. Overview of ChatGPT and BARD:
-ChatGPT is a type of artificial intelligence language model that is designed to respond to text-based inputs in a conversational manner. It uses deep learning algorithms to generate responses based on the patterns it has seen in large amounts of text data, and it can be used to build chatbots, answer questions, and perform other language-related tasks.
-
-In simple terms, ChatGPT is like a virtual assistant that can understand and respond to written language. Its goal is to simulate a human-like conversation, allowing users to interact with it in a way that feels natural and intuitive. The technology behind ChatGPT is complex, but the idea is to make it easy for people to use and benefit from it in their daily lives.
-
-You can access ChatGPT from OpenAI official site, click here ChatGPT'''
 
 #sidebar for user input
-
 with st.sidebar:
     st.title("Input Your Blog Details")
     st.subheader("Enter Details of Blog You want to generate")
@@ -43,10 +62,14 @@ with st.sidebar:
     #Number of images
     num_images = st.number_input("Number of Images", min_value=0, max_value=5,step=1)
 
+    prompt_parts= [
+        f"generate a comprehensivee, engaging blog post relevant to the given title \"{blog_title}\" and keywords \"{keywords}\". make sure to incorporate these keywords in the blog post. the blog should be approximately {num_words} words in length, suitable for an online audience. ensure the content is original, informative, and maintains a consist tone throughout."]
+
+    response = model.generate_content(prompt_parts)
     #submit button
     submit_button = st.button("Generate Blog")
 
 if submit_button:
-    st.image("https://avatars.githubusercontent.com/u/86125144?v=4")
+    # st.image("https://avatars.githubusercontent.com/u/86125144?v=4")
 
-    st.write(blog)
+    st.write(response.text)
